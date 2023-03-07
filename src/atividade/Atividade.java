73cef9b783d7f47b10a3df3e80632237661690c8
Gabriel Sardinha Ferreira <gabrielsardinhaferreira@gmail.com>
2023-03-07 17:17:49 -0300

    Alteracoes basicas

--- src/atividade/Atividade.java
+++ src/atividade/Atividade.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class Atividade {
 	
 	public static void main(String[] args) {
-	
 
-		
+//		1- Escreva um programa que imprima os números de 1 a 100. Mas para múltiplos de três imprima 'Tico' em vez do número e para os múltiplos de cinco imprima 'Teco'.
+//		Para números que são múltiplos de três e cinco imprimir 'TicoTeco'.
+//		Exemplo, para o caso de 1 a 5, o programa nos daria '1, 2, Tico, 4, Teco'.
+//		Utilize o esqueleto a seguir para implementar seu código:
 		
 		ticoTeco();
 		
-		
-		
-		
+//		2- Um palíndromo é uma palavra que pode ser lida da esquerda para a direita e vice-versa.
+//		Escreva uma função que verifica se a palavra dada é um palíndromo.
+//		Caracteres em caixa alta devem ser ignorados.
+//		Por exemplo, isPalindrome("Deleveled") deve retornar True, já que é um palíndromo por ser possível ser lido nos dois sentidos.
+//		Utilize o esqueleto a seguir para implementar seu código:
 		
 		System.out.println("Digite uma palavra: ");
 		Scanner scan = new Scanner(System.in);
-		
 		String palavra = scan.nextLine();
-		
         boolean resposta = isPalindrome(palavra);
-
         scan.close();
         
         if(resposta) {
@@ -31,6 +32,8 @@ public class Atividade {
         }
         
 	}
+	
+	
 	
 	public static void ticoTeco(){
        
@@ -53,6 +56,8 @@ public class Atividade {
             }
         }
     }
+	
+	
 	
 	public static boolean isPalindrome(String s) {
 		s = s.toLowerCase();
